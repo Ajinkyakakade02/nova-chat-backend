ab1102e66d3d764e7e93ffd305e67ec013958a31
Ajinkya kakade <ajinkyakakde510@gmail.com>
2026-08-17 18:16:31 +0530

    Create MailService.java

---
+++ src/main/java/com/chat/app/service/MailService.java
@@ -0,0 +1,34 @@
+package com.chat.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom("YOUR_EMAIL@gmail.com");
+        mailSender.send(message);
+    }
+
+    public void sendOtpEmail(String to, String otp) {
+        String subject = "NovaChat - Your OTP Code";
+        String body = "Your OTP code is: " + otp + "\n\nThis code will expire in 5 minutes.";
+        sendEmail(to, subject, body);
+    }
+
+    public void sendWelcomeEmail(String to, String name) {
+        String subject = "Welcome to NovaChat!";
+        String body = "Hi " + name + ",\n\nWelcome to NovaChat! Your account has been created successfully.\n\nHappy chatting!";
+        sendEmail(to, subject, body);
+    }
+}
